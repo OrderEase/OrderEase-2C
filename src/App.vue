11239053d048949c6d15b69744c398fad16a2480
James <874244887@qq.com>
2018-04-25 17:35:19 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tabbar>
+        <tabbar-item>
+            <span slot="label">菜单</span>
+        </tabbar-item>
+        <tabbar-item>
+            <span slot="label">购物车</span>
+        </tabbar-item>
+        <tabbar-item>
+            <span slot="label">订单</span>
+        </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vux'
+
+export default {
+  name: 'app',
+  components: {
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
