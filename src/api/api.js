f2ad32dbde442dd0b3f8a936dc42ab8981ea8d67
James <874244887@qq.com>
2018-06-28 10:50:40 +0800

    add order api

--- src/api/api.js
+++ src/api/api.js
@@ -6,4 +6,75 @@ let Promotion = {}
 let Order = {}
 let Restaurant = {}
 
+// 用户获取全部订单 
+Order.getAll = () => {
+  return new Promise((resolve, reject) => {
+    axios.get('/orders/cuser/')
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          if (error.response) {
+            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
+            reject(new Error(error.response.data.message))
+          } else {
+            reject(error)
+          }
+        })
+  })
+}
+
+// 用户修改订单消息
+Order.modifyOrderInfo = (orderId, orderInfo) => {
+  return new Promise((resolve, reject) => {
+    axios.put('/orders/cuser/oid/' + orderId, orderInfo)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          if (error.response) {
+            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
+            reject(new Error(error.response.data.message))
+          } else {
+            reject(error)
+          }
+        })
+  })
+}
+
+Order.submitOrder = (orderInfo) => {
+  return new Promise((resolve, reject) => {
+    axios.post('/orders/cuser/', orderInfo)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          if (error.response) {
+            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
+            reject(new Error(error.response.data.message))
+          } else {
+            reject(error)
+          }
+        })
+  })
+}
+
+// 订单付款
+Order.pay = (modifiedOrderId, payId) => {
+  return new Promise((resolve, reject) => {
+    axios.post('/orders/cuser/oid/' + modifiedOrderId, payId)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          if (error.response) {
+            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
+            reject(new Error(error.response.data.message))
+          } else {
+            reject(error)
+          }
+        })
+  })
+}
+
 export { User, Menu, Promotion, Order, Restaurant }
