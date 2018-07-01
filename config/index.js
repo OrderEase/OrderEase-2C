5f9da70d8361426f7cbf0bca0b2ee52502d5be46
XQ <837456906@qq.com>
2018-07-01 10:30:07 +0800

    revise something

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://orderease.github.io/OrderEase-2C/',
 
     /**
      * Source Maps
